6cfae196372d661e711862658debb4762109f4c2
igorx8 <igor.carvalhox8@gmail.com>
2025-02-05 20:20:30 -0300

    handling errors + async errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,8 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import "express-async-errors";
 
 const app = express();
 
@@ -15,6 +17,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,19 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
-export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (err instanceof RequestValidationError) {
-    console.log("validation");
+export const errorHandler = (err: Error, req: Request, res: Response) => {
+  if (err instanceof CustomError) {
+    res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    return;
   }
 
-  if (err instanceof DatabaseConnectionError) {
-    console.log("db error");
-  }
-  res.status(400).send({ message: err.message });
+  res.status(400).send({ errors: [{ message: "Something went wrong" }] });
+  return;
 };
