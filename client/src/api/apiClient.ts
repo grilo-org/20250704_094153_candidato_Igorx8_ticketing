5352c996bdf12572893616c83c316a44ca08da00
igorx8 <igor.carvalhox8@gmail.com>
2025-04-25 16:39:29 -0300

    feat: implement apiClient for HTTP requests and session management; remove deprecated buildClient

---
+++ client/src/api/apiClient.ts
@@ -0,0 +1,43 @@
+import { cookies } from "next/headers";
+
+type HttpMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+interface ApiClientOptions extends RequestInit {
+  method?: HttpMethod;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  body?: any;
+  cache?: RequestCache;
+  next?: {
+    revalidate?: number | false;
+    tags?: string[];
+  };
+}
+
+export async function apiClient<T>(
+  path: string,
+  options: ApiClientOptions = {}
+): Promise<T> {
+  const { method = "GET", body, headers, ...rest } = options;
+
+  const cks = await cookies();
+
+  const session = cks.get("session")?.value;
+
+  const response = await fetch(`http://easytickets.dev${path}`, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+      Cookie: `session=${session}`,
+    },
+    body: body ? JSON.stringify(body) : undefined,
+    ...rest,
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`Erro ${response.status}: ${errorText}`);
+  }
+
+  return response.json();
+}
